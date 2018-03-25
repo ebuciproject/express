51816aada8fd65647a83db411f7441e74cf3214a
ebuciproject <ebuciproject@gmail.com>
2018-03-25 21:25:27 +0600

    Second commit with new functionality. Addding new module fortune

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-var express, app, handlebars, fortunes;
+var express, app, handlebars, fortune;
 
 express = require('express');
 app = express();
 handlebars = require('express3-handlebars')
   .create({defaultLayout:'main'});
+fortune = require('./lib/fortune.js')
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -11,22 +12,12 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 3000);
 
-fortunes = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.",
-  "Do not fear what you don't know.",
-  "You will have a pleasant surprise.",
-  "Whenever possible, keep it simple.",
-];
-
 app.get('/', function(req, res) {
   res.render('home');
 });
 
 app.get('/about', function(req, res) {
-  var randomFortune =
-    fortunes[Math.floor(Math.random() * fortunes.length)];
-  res.render('about', { fortune: randomFortune });
+  res.render('about', { fortune: fortune.getFortune() });
 });
 
 // Custom 404 page
